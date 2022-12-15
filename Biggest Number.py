0479c45dfe4c27c65a2c6aac5a42e088d5ce94fc
Roselyn <103125753+C-ROSELYN@users.noreply.github.com>
2022-12-15 23:27:45 +0530

    Add files via upload

---
+++ Biggest Number.py
@@ -0,0 +1,12 @@
+
+#greatest number among two numbers
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+if(a>b):
+    print(a,"is the greatest number")
+elif(a==b):
+    print("both are equal")
+else:
+    print(b,"is the greatest number")
+    
+input()    
